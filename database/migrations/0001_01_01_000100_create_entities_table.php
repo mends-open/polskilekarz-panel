5fb8e0c0bc223623e01e0d30ed639d87ee9febde
dominik-mukrecki <admin@mends.eu>
2025-09-07 15:28:38 +0200

    Remove `footers` column from `entities` table and add `entity_id` foreign key to `documents` table

--- database/migrations/0001_01_01_000100_create_entities_table.php
+++ database/migrations/0001_01_01_000100_create_entities_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->text('name');
             $table->jsonb('headers')->nullable();
-            $table->jsonb('footers')->nullable();
             $table->timestampsTz();
             $table->softDeletesTz();
         });
