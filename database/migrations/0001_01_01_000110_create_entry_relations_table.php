8db4ed9a7db656057b484b37996a59d038ca6fc9
dominik-mukrecki <admin@mends.eu>
2025-09-09 23:24:00 +0200

    Remove `appointments` table migration and add `entry_relations` table migration

---
+++ database/migrations/0001_01_01_000110_create_entry_relations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Entry;
+use Illuminate\Database\Migrations\Migration;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entry_relations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Entry::class)->constrained();
+            $table->bigInteger('related_id');
+            $table->smallInteger('related_type');
+            $table->timestampsTz();
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entry_relations');
+    }
+};
