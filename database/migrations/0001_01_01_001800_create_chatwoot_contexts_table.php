484f4be852096237ba38e89cfa4cc6c0490261ab
dominik-mukrecki <admin@mends.eu>
2025-09-10 00:46:14 +0200

    Create `chatwoot_contexts` table and corresponding `ChatwootContext` model for managing Chatwoot-related data

---
+++ database/migrations/0001_01_01_001800_create_chatwoot_contexts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chatwoot_contexts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('contextable_id');
+            $table->smallInteger('contextable_type');
+            $table->bigInteger('chatwoot_account_id');;
+            $table->bigInteger('chatwoot_conversation_id');
+            $table->bigInteger('chatwoot_contact_id');
+            $table->bigInteger('chatwoot_user_id');
+            $table->timestampsTz();
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chatwoot_contexts');
+    }
+};
