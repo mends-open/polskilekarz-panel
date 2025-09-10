86d9575d4683a43dc0d3a0e209f15a80bcd73a44
dominik-mukrecki <admin@mends.eu>
2025-09-10 11:28:25 +0200

    Update migrations to use `Tpetry\PostgresqlEnhanced` and add timezone-aware timestamps and soft deletes

--- database/migrations/0001_01_01_001900_create_short_links_table.php
+++ database/migrations/0001_01_01_001900_create_short_links_table.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('short_links', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestampsTz();
+            $table->softDeletesTz();
         });
     }
 
