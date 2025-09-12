f306663862af35863e2b381cd1a7fb8f3569838f
mends-open <admin+open@mends.eu>
2025-09-12 13:49:05 +0200

    Refactor Cloudflare service for conditional link creation

--- app/Services/CloudflareService.php
+++ app/Services/CloudflareService.php
@@ -21,49 +21,65 @@ class CloudflareService
         $this->shortenerDomain = config('services.cloudflare.shortener_domain');
     }
 
-    public function put(string $key, string $value): void
+    protected function kvUrl(string $key): string
     {
-        Http::withToken($this->apiToken)
-            ->put(
-                sprintf(
-                    '%s/accounts/%s/storage/kv/namespaces/%s/values/%s',
-                    $this->endpoint,
-                    $this->accountId,
-                    $this->namespaceId,
-                    $key
-                ),
-                $value
-            )->throw();
+        return sprintf(
+            '%s/accounts/%s/storage/kv/namespaces/%s/values/%s',
+            $this->endpoint,
+            $this->accountId,
+            $this->namespaceId,
+            $key
+        );
     }
 
-    public function get(string $key): string
+    public function create(string $key, string $value): array
     {
-        return Http::withToken($this->apiToken)
-            ->get(
-                sprintf(
-                    '%s/accounts/%s/storage/kv/namespaces/%s/values/%s',
-                    $this->endpoint,
-                    $this->accountId,
-                    $this->namespaceId,
-                    $key
-                )
-            )
-            ->throw()
-            ->body();
+        $response = Http::withToken($this->apiToken)
+            ->withHeaders([
+                'Content-Type' => 'text/plain',
+                'If-None-Match' => '*',
+            ])
+            ->send('PUT', $this->kvUrl($key), ['body' => $value]);
+
+        if ($response->successful()) {
+            return [
+                'success' => true,
+                'created' => true,
+                'short_url' => $this->shortUrl($key),
+                'value' => $value,
+            ];
+        }
+
+        if ($response->status() === 412) {
+            $existing = $this->get($key);
+
+            return [
+                'success' => true,
+                'created' => false,
+                'short_url' => $this->shortUrl($key),
+                'value' => $existing,
+            ];
+        }
+
+        return ['success' => false, 'created' => false];
     }
 
-    public function delete(string $key): void
+    public function get(string $key): ?string
     {
-        Http::withToken($this->apiToken)
-            ->delete(
-                sprintf(
-                    '%s/accounts/%s/storage/kv/namespaces/%s/values/%s',
-                    $this->endpoint,
-                    $this->accountId,
-                    $this->namespaceId,
-                    $key
-                )
-            )->throw();
+        $response = Http::withToken($this->apiToken)->get($this->kvUrl($key));
+
+        if ($response->successful()) {
+            return $response->body();
+        }
+
+        return null;
+    }
+
+    public function delete(string $key): bool
+    {
+        $response = Http::withToken($this->apiToken)->delete($this->kvUrl($key));
+
+        return $response->successful();
     }
 
     public function shortUrl(string $key): string
@@ -71,4 +87,3 @@ class CloudflareService
         return rtrim($this->shortenerDomain, '/') . '/' . $key;
     }
 }
-
