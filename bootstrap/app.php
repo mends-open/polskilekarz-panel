a0407e2b043609e3845683068ed93836f274aac2
dominik-mukrecki <admin@mends.eu>
2025-09-12 11:16:20 +0200

    Remove redundant `stripe/events/*` exemption from CSRF middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'stripe/events',
-            'stripe/events/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
