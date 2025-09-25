fdda313f6886b3a3964637db01a56d5824c72241
dominik-mukrecki <admin@mends.eu>
2025-09-25 12:51:11 +0200

    Rename `services.cloudflare.links` keys to `services.cloudflare.shortener` for improved clarity and add new `entries_namespace_id` key.

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 12.30.1.
+ * Generated for Laravel 12.31.1.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
