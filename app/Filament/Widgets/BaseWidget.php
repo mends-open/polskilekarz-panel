5c17fb796a05a407cb3502b43c4a0cbdc464166f
mends-open <admin+open@mends.eu>
2025-09-30 02:25:24 +0200

    Ensure base widget implements Filament actions contract

--- app/Filament/Widgets/BaseWidget.php
+++ app/Filament/Widgets/BaseWidget.php
@@ -3,11 +3,12 @@
 namespace App\Filament\Widgets;
 
 use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
 use Filament\Widgets\Widget;
 
-class BaseWidget extends Widget implements HasSchemas
+class BaseWidget extends Widget implements HasSchemas, HasActions
 {
     use InteractsWithActions;
     use InteractsWithSchemas;
