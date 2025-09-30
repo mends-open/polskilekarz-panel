19aa7c308e1999b314524bf3ef6f93a7310d46b2
mends-open <admin+open@mends.eu>
2025-09-30 02:19:17 +0200

    Fix action handling on base widget

--- app/Filament/Widgets/BaseWidget.php
+++ app/Filament/Widgets/BaseWidget.php
@@ -2,12 +2,14 @@
 
 namespace App\Filament\Widgets;
 
+use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
 use Filament\Widgets\Widget;
 
 class BaseWidget extends Widget implements HasSchemas
 {
+    use InteractsWithActions;
     use InteractsWithSchemas;
 
     protected string $view = 'filament.widgets.base-widget';
